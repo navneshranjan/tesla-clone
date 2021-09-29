cd3a66abe30084b6300924d2b11bc8fa51caf81c
navneshranjan <navneshranjan@gmail.com>
2021-09-29 14:17:03 +0530

    [Model S page]

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+
+function Home() {
+  return (
+    <container>
+      <Section />
+    </container>
+  );
+}
+
+export default Home;
+const container = styled.div`
+height=100vh;
+`;
