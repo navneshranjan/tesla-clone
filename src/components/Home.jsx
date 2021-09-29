d831359457844f13dc0b3d1536c04830f24dcc60
navneshranjan <navneshranjan@gmail.com>
2021-09-29 16:54:59 +0530

    [all sections added with button]

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,54 @@ import Section from "./Section";
 function Home() {
   return (
     <container>
-      <Section />
+      <Section
+        title="Model S"
+        description="Order Online For Touchless Delevery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom Online"
+        rightBtnText="Existing Enventery"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online For Touchless Delevery"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom Online"
+        rightBtnText="Existing Enventery"
+      />
+      <Section
+        title="Model 3"
+        description="Order  For Touchless Delevery"
+        backgroundImg="model-3.jpg"
+        leftBtnText="Custom Online"
+        rightBtnText="Existing Enventery"
+      />
+      <Section
+        title="Model X"
+        description="Order  For Touchless Delevery"
+        backgroundImg="model-x.jpg"
+        leftBtnText="Custom Online"
+        rightBtnText="Existing Enventery"
+      />
+      <Section
+        title="Lowest cost Solar Pannel"
+        description="Monet-Back Guarentee"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="know more"
+      />
+      <Section
+        title="Solar for New Roof"
+        description="Solar roof cost less than new roof"
+        backgroundImg="solar-roof.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="know more"
+      />
+      <Section
+        title="Accessories"
+        description=""
+        backgroundImg="accessories.jpg"
+        leftBtnText="Order Now"
+      />
     </container>
   );
 }
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 
-function Section() {
+function Section({
+  title,
+  description,
+  backgroundImg,
+  leftBtnText,
+  rightBtnText,
+}) {
   return (
-    <Wrap>
+    <Wrap bgImage={backgroundImg}>
       <ItemText>
-        <h1>Model S</h1>
-        <p>Order Online For Touchless Delevery</p>
+        <h1>{title}</h1>
+        <p>{description}</p>
       </ItemText>
       <Buttons>
         <ButtonGroup>
-          <LeftButton>Custom Order</LeftButton>
-          <RightButton>Existing Enventory</RightButton>
+          <LeftButton>{leftBtnText}</LeftButton>
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+          {/* this is conditional button if right btn text exist then only show the right btn otherwise only show left btn at center */}
         </ButtonGroup>
         <DownArrow src="/images/down-arrow.svg" />
       </Buttons>
@@ -28,11 +35,11 @@ const Wrap = styled.div`
   background-position: center;
   background-repeat: no-repeat;
 
-  background-image: url(/images/model-s.jpg);
   display: flex;
   flex-direction: column;
   justify-content: space-between; //vertical align
   align-items: center;
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
 `;
 const ItemText = styled.div`
   padding-top: 15vh;
@@ -41,6 +48,9 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
   display: flex;
   margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  } ;
 `;
 const LeftButton = styled.div`
   background-color: rgba(23, 26, 32, 0.8);
@@ -59,7 +69,11 @@ margin:8px;
 
 
   `;
-const RightButton = styled(LeftButton)``;
+const RightButton = styled(LeftButton)`
+  background: white;
+  opacity: 0.65;
+  color: black;
+`;
 
 const DownArrow = styled.img`
   margin-top: 20px;
